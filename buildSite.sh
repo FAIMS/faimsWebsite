cc2ccb0ae56dee8783915c2aaf7f27844be37c3a
Brian Ballsun-Stanton <brian@fedarch.org>
2016-01-15 16:29:22 +1100

    typo

--- buildSite.sh
+++ buildSite.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -euo pipefail
+source ~/.bashrc
 
-echo "Deploying"
+echo "Building"
 
 
 cd $HOME/faimsWebsite
 git pull > /tmp/gitlog 2> /tmp/giterr
 jekyll build --destination $HOME/faims.github.io/ >> /tmp/gitlog 2>> /tmp/giterr
-
+echo "Deploying"
 cd $HOME/faims.github.io
-git add --all $HOME/faims/github.io >> /tmp/gitlog 2>> /tmp/giterr
+git add --all $HOME/faims.github.io >> /tmp/gitlog 2>> /tmp/giterr
 git commit -a -m "automatic commit $(date)">> /tmp/gitlog 2>> /tmp/giterr
 git push -f >> /tmp/gitlog 2>> /tmp/giterr
 
@@ -17,4 +18,3 @@ cat /tmp/gitlog
 cat /tmp/giterr
 
 echo "Done"
-
