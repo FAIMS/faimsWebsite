95e3650f222a38e54d8e7539a215de8baa3d8ad8
Brian Ballsun-Stanton <brian@fedarch.org>
2016-01-15 14:48:52 +1100

    builder.sh

--- buildSite.sh
+++ buildSite.sh
@@ -6,8 +6,9 @@ cd $HOME/faims.github.io
 git pull
 
 cd $HOME/faimsWebsite
+git pull
+jekyll clean
 jekyll build
-cp -r $HOME/faimsWebsite/_site/* $HOME/faims.github.io/
 
 cd $HOME/faims.github.io
 git commit -a -m "automatic commit $(date)"
