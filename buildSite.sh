f07917c45538d40976b1ce1b372f9be5618af82b
Brian Ballsun-Stanton <brian@fedarch.org>
2016-01-15 15:56:34 +1100

    buildfix

--- buildSite.sh
+++ buildSite.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+set -euo pipefail
 
 echo "Deploying"
 
 cd $HOME/faimsWebsite
-git pull
-jekyll clean
-jekyll build
+git pull > /tmp/gitlog 2> /tmp/giterr
+jekyll clean >> /tmp/gitlog 2>> /tmp/giterr
+jekyll build >> /tmp/gitlog 2>> /tmp/giterr
 
 cd $HOME/faims.github.io
-git add -Au *
-git commit -a -m "automatic commit $(date)" --allow-empty 
-git push
+git add -A * >> /tmp/gitlog 2>> /tmp/giterr
+git commit -a -m "automatic commit $(date)" --allow-empty >> /tmp/gitlog 2>> /tmp/giterr
+git push >> /tmp/gitlog 2>> /tmp/giterr
+
+cat /tmp/gitlog
+cat /tmp/giterr
+
+echo "Done"
+
