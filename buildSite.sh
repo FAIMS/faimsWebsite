42116f10f5a38760c7f359c0a1ce0471799e3b64
Brian Ballsun-Stanton <brian@fedarch.org>
2016-01-15 16:12:22 +1100

    whee

--- buildSite.sh
+++ buildSite.sh
@@ -3,14 +3,15 @@ set -euo pipefail
 
 echo "Deploying"
 
+
 cd $HOME/faimsWebsite
 git pull > /tmp/gitlog 2> /tmp/giterr
 jekyll build --destination $HOME/faims.github.io/ >> /tmp/gitlog 2>> /tmp/giterr
 
 cd $HOME/faims.github.io
-git add -A >> /tmp/gitlog 2>> /tmp/giterr
-git commit -a -m "automatic commit $(date)" git>> /tmp/gitlog 2>> /tmp/giterr
-git push >> /tmp/gitlog 2>> /tmp/giterr
+git add --all $HOME/faims/github.io >> /tmp/gitlog 2>> /tmp/giterr
+git commit -a -m "automatic commit $(date)">> /tmp/gitlog 2>> /tmp/giterr
+git push -f >> /tmp/gitlog 2>> /tmp/giterr
 
 cat /tmp/gitlog
 cat /tmp/giterr
