574ef9a35ba5f630adc5c5642d8541b6470196ce
Brian Ballsun-Stanton <brian@fedarch.org>
2016-01-15 16:06:49 +1100

    again

--- buildSite.sh
+++ buildSite.sh
@@ -5,12 +5,11 @@ echo "Deploying"
 
 cd $HOME/faimsWebsite
 git pull > /tmp/gitlog 2> /tmp/giterr
-jekyll clean >> /tmp/gitlog 2>> /tmp/giterr
-jekyll build >> /tmp/gitlog 2>> /tmp/giterr
+jekyll build --destination $HOME/faims.github.io/ >> /tmp/gitlog 2>> /tmp/giterr
 
 cd $HOME/faims.github.io
 git add -A * >> /tmp/gitlog 2>> /tmp/giterr
-git commit -a -m "automatic commit $(date)" --allow-empty >> /tmp/gitlog 2>> /tmp/giterr
+git commit -a -m "automatic commit $(date)" git>> /tmp/gitlog 2>> /tmp/giterr
 git push >> /tmp/gitlog 2>> /tmp/giterr
 
 cat /tmp/gitlog
