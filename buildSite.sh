ea3a54ddfdf1bee1858dbe08270d71d0883fa6b9
Brian Ballsun-Stanton <brian@fedarch.org>
2016-01-15 16:09:49 +1100

    one more time

--- buildSite.sh
+++ buildSite.sh
@@ -8,7 +8,7 @@ git pull > /tmp/gitlog 2> /tmp/giterr
 jekyll build --destination $HOME/faims.github.io/ >> /tmp/gitlog 2>> /tmp/giterr
 
 cd $HOME/faims.github.io
-git add -A * >> /tmp/gitlog 2>> /tmp/giterr
+git add -A >> /tmp/gitlog 2>> /tmp/giterr
 git commit -a -m "automatic commit $(date)" git>> /tmp/gitlog 2>> /tmp/giterr
 git push >> /tmp/gitlog 2>> /tmp/giterr
 
