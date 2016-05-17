4a5a591dfefc085df6a56f095231a7cffa33b466
Brian Ballsun-Stanton <brian@fedarch.org>
2016-05-17 14:58:43 +1000

    sorted posts by year

---
+++ makePost.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+date=$(date +"%Y-%m-%d %H:%M:%S %z")
+
+cat << EndOfMessage
+---
+author: denubis
+date: $date
+layout: post
+slug: 
+title: 
+
+categories:
+- News
+---
+EndOfMessage
