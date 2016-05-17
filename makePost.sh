06ca586782b2a80c13d863823751234e0fa2e873
Brian Ballsun-Stanton <brian@fedarch.org>
2016-05-17 17:18:13 +1000

    sidebar

--- makePost.sh
+++ makePost.sh
@@ -2,16 +2,27 @@
 
 
 date=$(date +"%Y-%m-%d %H:%M:%S %z")
+title=$*
+fileTitle=$(echo $(date +"%Y-%m-%d")"$title" | sed 's/ /-/g')
+echo $title $fileTitle
 
 cat << EndOfMessage
 ---
 author: denubis
 date: $date
 layout: post
-slug: 
-title: 
-
+title: $title
+subheadline: 
+teaser: 
+breadcrumb: true
 categories:
-- News
+    - News
+tags:
+    - blog
+
+image:
+    title: 
+    caption: 
+    caption_url: 
 ---
 EndOfMessage
