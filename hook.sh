fcbdb06cbfd3fe346073457f2a5addd8bb2ade90
Brian Ballsun-Stanton <brian@fedarch.org>
2016-06-23 17:33:02 +1000

    update hook.sh

--- hook.sh
+++ hook.sh
@@ -3,7 +3,13 @@
 source /home/fedarch/.bashrc
 
 cd /home/fedarch/faimsWebsite; 
-git pull; > /tmp/hook.log
-/home/fedarch/.rbenv/shims/jekyll build &>> /tmp/hook.log
 
-python ~/logToSlack/hook.sh /tmp/hook.log
+git log -n 1 &> /tmp/header.log
+
+git pull &> /tmp/git.log
+echo $? > /tmp/gitStatus.log
+
+/home/fedarch/.rbenv/shims/jekyll build &> /tmp/jekyll.log
+echo $? > /tmp/jekyllStatus.log
+
+python ~/logToSlack/logToSlack.py 
