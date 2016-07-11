a3072f19fe25dcfa3c6c1d9e3315ef3b277f53e9
Brian Ballsun-Stanton <brian@fedarch.org>
2016-07-11 15:45:27 +1000

    Jekyll Build

--- hook.sh
+++ hook.sh
@@ -4,12 +4,18 @@ source /home/fedarch/.bashrc
 
 cd /home/fedarch/faimsWebsite; 
 
-git log -n 1 &> /tmp/header.log
+git log -n 2 &> /tmp/header.log
 
 git pull &> /tmp/git.log
 echo $? > /tmp/gitStatus.log
 
 /home/fedarch/.rbenv/shims/jekyll build &> /tmp/jekyll.log
 echo $? > /tmp/jekyllStatus.log
+
+git add -A >> /tmp/git.log
+
+git commit -a -m "Jekyll Build" >> /tmp/git.log
+
+git push >> /tmp/git.log
 
 python ~/logToSlack/logToSlack.py 
